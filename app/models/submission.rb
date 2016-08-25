dd5bf39bf5c4c7b4823176b050737bc2c9d8a94d
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2016-08-25 15:47:18 -0400

    Copy fatsecret api from github gist example and get rid of all the methods and start consolidating

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,9 @@
 require 'uri'
 require 'open-uri'
+require 'openssl'
+require 'base64'
+require 'digest/md5'
+require 'net/http'
 
 class Submission < ApplicationRecord
   serialize :spoon_recipe_response
@@ -8,10 +12,40 @@ class Submission < ApplicationRecord
   before_save :getTitleUrl, :downcase_attributes, :smart_add_url_protocol, :get_recipe_from_spoon
 
   def self.fatsecret_ingredient_lookup(id)
+    secret = ENV['FATSECRET_CONSUMER_SHARED_SECRET']
+    http_method = 'GET'
+    request_url = "http://platform.fatsecret.com/rest/server.api"
+    params = {
+        :oauth_consumer_key => ENV['FATSECRET_CONSUMER_API_KEY'],
+        :oauth_nonce => Digest::MD5.hexdigest(rand(11).to_s),
+        :oauth_signature_method => "HMAC-SHA1",
+        :oauth_timestamp => Time.now.to_i,
+        :oauth_version => "1.0",
+        :method => 'foods.search',
+        :search_expression => expression
+    }
+    sorted_params = params.sort {|a, b| a.first.to_s <=> b.first.to_s}
+    param_str = sorted_params.collect{|pair| "#{pair.first}=#{pair.last}"}.join('&')
+
     ingredients_array = Submission.find(id).spoon_recipe_response
     ingredients_array["extendedIngredients"].map{|hash| hash["name"]}
+
     ingredients_array.each do |ingredient|
-      # API call to fatsecret for each ingredient. Return the JSON and parse out nutrition facts.
+
+      list = [http_method.esc, request_url.esc, param_str.esc]
+      base = list.join("&")
+      pairs = params.sort {|a, b| a.first.to_s <=> b.first.to_s}
+      list = []
+      pairs.inject(list) {|arr, pair| arr << "#{pair.first.to_s}=#{pair.last}"}
+      http_params = list.join("&")
+      token = ''
+      secret = "#{SECRET.esc}&#{token.esc}"
+      sign = Base64.encode64(OpenSSL::HMAC.digest('sha1',secret, base)).gsub(/\n/,'')
+      sig = CGI.escape(sign).gsub("%7E", "~").gsub("+", "%20")
+      parts = http_params.split('&')
+      parts << "oauth_signature=#{sig}"
+      uri = URI.parse("#{request_url}?#{parts.join('&')}")
+      results = Net::HTTP.get(uri)
     end
     return "nutrition facts"
   end
